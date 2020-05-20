2cbe858a31a7146a4212593be1bc327fd143addb
Oleg <example@gmail.com>
2020-05-20 18:28:28 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+"use strict";
+
+//7. Простое общение с пользователем
+/*alert('Hello');
+
+const result = confirm('Are you here?');
+console.log(result);
+
+const answer = +prompt('Вам уже есть 18 лет?','18');
+console.log(answer + 5);
+
+const answers = [];
+answers[0] = prompt('Как Ваше имя?','');
+answers[1] = prompt('Как Ваше фамилия?','');
+answers[2] = prompt('Сколько Вам лет?','');
+
+document.write(answers);*/
+
+//8.Интерполяция (ES6)
+const category = 'toys';
+console.log(`https://someurl.com/${category}/5`);
+
+const user = 'Julia';
+alert(`Привет, ${user}`);
